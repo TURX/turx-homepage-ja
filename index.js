d0f5c1de6e71293ef9162c6ea39789098a202284
TURX <turx2003@gmail.com>
2021-10-24 18:44:35 -0500

    English desktop site

---
+++ index.js
@@ -0,0 +1,29 @@
+window.onload = function() {
+	document.body.className += " loaded";
+}
+
+$("#btn-email").click(function() {
+	window.location = "mailto:turx2003@gmail.com";
+});
+
+$("#btn-twitter").click(function() {
+	window.location = "https://twitter.com/_AruEkusu_";
+});
+
+$("#btn-blog").click(function() {
+	window.location = "https://blog.turx.asia";
+});
+
+$("#btn-github").click(function() {
+	window.location = "https://github.com/TURX";
+});
+
+$("#btn-resume").click(function() {
+	window.location = "https://direct.turx.asia/resume.pdf";
+});
+
+$("#bg").hover(function() {
+	$("#content").css("opacity", "0.7");
+}, function() {
+	$("#content").css("opacity", "1");
+});
